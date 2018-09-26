388d7c50063237a9f079fe03f7936822fed3b7a4
Ole Wehrmeyer <me@belogron.de>
2018-09-26 21:19:32 +0200

    kernel

--- loader.s
+++ loader.s
@@ -1,16 +1,31 @@
 global loader
 
-MAGIC_NUMBER 	equ 0x1BADB002
-FLAGS			equ 0x0
-CHECKSUM		equ -MAGIC_NUMBER
+MAGIC_NUMBER        equ 0x1BADB002
+FLAGS               equ 0x0
+CHECKSUM            equ -MAGIC_NUMBER
+
+KERNEL_STACK_SIZE   equ 4096
+
+
 
 section .text:
 align 4
-	dd MAGIC_NUMBER
-	dd FLAGS
-	dd CHECKSUM
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
 
 loader:
-	mov eax, 0xCAFEBABE
+    external kernel_main
+
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+
+    call kernel_main
+
 .loop:
-	jmp .loop
+    jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
+
