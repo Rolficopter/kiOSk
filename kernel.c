49789b0408ae3514b97e3e01f261e346cb67e63d
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2018-09-27 00:21:04 +0200

    Update keyboard input

--- kernel.c
+++ kernel.c
@@ -8,7 +8,7 @@ int kernel_main() {
 	write_with_colors(HELLO, 21, FB_COLOR_GREEN, FB_COLOR_BLACK);
 	while (1)
 	{
-		char c = read_keyboard_code();
+		char c = getc();
 		write(&c, 1);
 	}
 	return 0;
