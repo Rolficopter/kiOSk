129f2adc60a512b578ee05c4816502ac47726036
Ole Wehrmeyer <me@belogron.de>
2018-09-26 22:36:36 +0200

    fb

--- kernel.c
+++ kernel.c
@@ -1,9 +1,15 @@
 #include "beer/framebuffer.h"
+#include "beer/keyboard.h"
 
 static char HELLO[] = "Hallo Welt!!1!1elf!1!";
 
 int kernel_main() {
 	clear_screen();
 	write_with_colors(HELLO, 21, FB_COLOR_GREEN, FB_COLOR_BLACK);
+	while (1)
+	{
+		char c = getc();
+		write(&c, 1);
+	}
 	return 0;
 }
